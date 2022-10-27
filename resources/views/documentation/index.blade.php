af7f5353831a787b867ffa6b94add6d4c94216ab
natcast <nathalie.brooke@gmail.com>
2022-10-27 16:31:27 +0100

    Changed the about page

--- resources/views/documentation/index.blade.php
+++ resources/views/documentation/index.blade.php
@@ -2,28 +2,35 @@
 @section('content')
     <div class="card">
         <div class="card-header bg-blue-600 text-center">
-            <h1 class="text-4xl text-slate-100 text-bold justify-center p-9 ">About eRAcuration</h1>
+            <h1 class="text-bold justify-center p-9 text-4xl text-slate-100">About eRAcuration</h1>
         </div>
         <div class="card-body">
-            <h2 class="text-3xl uppercase font-extrabold pt-5  px-5">Goals</h2>
-            <h3 class="text-2xl font-bold pt-5  px-5 text-pink-900">One place for all the eRA and eRAdoc tools</h3>
-            <ul class="text-sm text-slate-900 pt-5 px-9 list-disc list-inside">
-                <li>Experiments: lists experiments and connect to GLTEN for editing metadata</li>
+            <h2 class="px-5 pt-5 text-3xl font-extrabold uppercase">Goals</h2>
+            <h3 class="px-5 pt-5 text-2xl font-bold text-pink-900">One place for all the eRA and eRAdoc tools</h3>
+            <h4 class="px-7 pt-7 text-xl font-bold text-slate-900">Done or In Progress</h4>
+            <ul class="list-inside list-disc px-9 pt-5 text-sm text-slate-900">
                 <li>Images: Manage images metadata</li>
                 <li>Datasets: Edit metadata for the Datasets and Documents DOIs </li>
-                <li>Data? Will that be used to assist Raw data?</li>
+                <li>Keywords - Subjects: add new subjects</li>
+            </ul>
+            <h4 class="px-7 pt-7 text-xl font-bold text-slate-900">Some Ideas</h4>
+            <ul class="list-inside list-disc px-9 pt-5 text-sm text-slate-900">
+
+                <li>Experiments: lists experiments and connect to GLTEN for editing metadata</li>
+
+                <li>Data - Will that be used to assist Raw data?</li>
                 <li>Users: view and manage eRAmanga - users having download privileges</li>
                 <li>Users: view and manage users requests obtained from eRAmanga</li>
                 <li>Users: view and manage users of eRAdata</li>
 
             </ul>
-            <h3 class="text-2xl font-bold pt-5  px-5 text-pink-900">Runs Python Programs</h3>
-            <ul class="text-sm text-slate-900 pt-5 px-9 list-disc list-inside">
+            <h3 class="px-5 pt-5 text-2xl font-bold text-pink-900">Runs Python Programs</h3>
+            <ul class="list-inside list-disc px-9 pt-5 text-sm text-slate-900">
                 <li>Update Metadata</li>
                 <li>Mint DOis</li>
                 <li>Update Metadata for Experiments</li>
             </ul>
-            <h3 class="text-2xl font-bold pt-5  px-5 text-pink-900">Documentation</h3>
+            <h3 class="px-5 pt-5 text-2xl font-bold text-pink-900">Documentation</h3>
         </div>
     </div>
 @endsection
