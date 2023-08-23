277a1e3cbbd838e2072a430b1aba01611e6cd9ce
Nathalie Castells-Brooke <36696807+nathcast@users.noreply.github.com>
2023-08-23 17:20:59 +0100

    make new from template - modified form and added validation

--- app/Http/Livewire/Add/DatasetFromTemplate.php
+++ app/Http/Livewire/Add/DatasetFromTemplate.php
@@ -9,21 +9,53 @@ class DatasetFromTemplate extends Component
     public $template_id;
     public $title;
     public $identifier = "10.23637/";
+    public $shortname;
+    public $exptCode;
+    public $url;
+    public $version;
 
     public function mount() {
+
         $template = Dataset::findOrFail($this->template_id);
-        $exptCode = str_replace("/", "",$template->experiment->code );
-        $shortname = $template->short_name;
-        $this->identifier = strtolower ("10.23637/".$exptCode."-".$shortname. "-".date('YmdHis'));
+        $this->version = $template->version + 1;
+        $this->exptCode = strtolower(str_replace("/", "",$template->experiment->code ));
+        $this->shortname = $template->short_name;
+        $this->identifier = "10.23637/".$this->exptCode.'-'.$this->shortname.'-'.$this->version;
+        if ($template->general_resource_type_id == 4)
+        {
+            $format = 'https://www.era.rothamsted.ac.uk/dataset/%1$s/%2$02d-%3$s';
+            $this->url = sprintf($format, strtolower($this->exptCode), $this->version, $this->shortname);
+        }
 
     }
 
     public function addDataset()
     {
         $template = Dataset::findOrFail($this->template_id);
+
+        if ($template->general_resource_type_id == 4)
+{
+    $format = 'https://www.era.rothamsted.ac.uk/dataset/%1$s/%2$02d-%3$s';
+    $this->url = sprintf($format, strtolower($this->exptCode), $this->version, $this->shortname);
+}
+else
+{
+
+}
+        $this->validate( [
+            'title' => 'required|max:100',
+            'identifier' =>['bail','required','doesnt_end_with:"10.23637/"','unique:App\Models\Dataset,identifier'],
+            'url' => ['unique:App\Models\Dataset,url']
+
+        ]);
+
+        $template = Dataset::findOrFail($this->template_id);
         $newDataset = $template->duplicate();
+        $newDataset->url = $this->url;
         $newDataset->identifier = $this->identifier;
         $newDataset->title = $this->title;
+        $newDataset->short_name = $this->shortname;
+        $newDataset->version = $this->version;
         $newDataset->is_ready = 1;
         //$newDataset->old_id = 1;
         $newDataset->doi_created = null;
--- lang/en/validation.php
+++ lang/en/validation.php
@@ -43,6 +43,7 @@ return [
     'digits_between' => 'The :attribute must be between :min and :max digits.',
     'dimensions' => 'The :attribute has invalid image dimensions.',
     'distinct' => 'The :attribute field has a duplicate value.',
+    'doesnt_end_with' => 'The :attribute field must not end with :value .',
     'email' => 'The :attribute must be a valid email address.',
     'ends_with' => 'The :attribute must end with one of the following: :values.',
     'enum' => 'The selected :attribute is invalid.',
